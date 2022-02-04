b003265addea0887e94c3080b6449dd7c275d7a9
Matt Haines <mhpahuta@gmail.com>
2022-02-04 14:26:08 -0800

    Add get and display repo functions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 // target overview div - where profile info will appear
 const overview = document.querySelector('.overview');
+const repoList = document.querySelector('.repo-list');
+
 const username = 'MattPahuta';
 
 // fetch data from GitHub api
@@ -19,7 +21,6 @@ const fetchGitHubData = async function() {
 fetchGitHubData();
 
 
-
 const displayUserInfo = data => {
   const newDiv = document.createElement('div');
   newDiv.classList.add('user-info');
@@ -34,5 +35,26 @@ const displayUserInfo = data => {
       <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
     </div>`
   overview.append(newDiv);
+  fetchRepos()
+}
 
+const fetchRepos = async function() {
+  // fetch user's repos - list repos of user
+  /* parameters - 
+    - sort by most recently added to last updated - ?sort=updated
+    - show up to 100 repos per page - &per_page=100
+  */
+  const request = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+  const data = await request.json();
+  console.log(data);
+  displayRepoInfo(data);
+}
+
+const displayRepoInfo = repos => {
+  for (repo of repos) {
+    const li = document.createElement('li');
+    li.classList.add('repo');
+    li.innerHTML = `<h3>${repo.name}</h3>`
+    repoList.append(li);
+  }
 }
