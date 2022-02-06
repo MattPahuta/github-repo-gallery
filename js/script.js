b54227c4012dd6adb48338e9613af372be71f98d
Matt Haines <mhpahuta@gmail.com>
2022-02-06 14:17:06 -0800

    Add dynamic repo search

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 // target overview div - where profile info will appear
 const overview = document.querySelector('.overview'); // repo div
 const repoList = document.querySelector('.repo-list'); // repo ul
-const repos = document.querySelector('.repos'); // repos section
+const allReposContainer = document.querySelector('.repos'); // repos section
 const repoData = document.querySelector('.repo-data'); // repo data section
+const backBtn = document.querySelector('.view-repos'); // back to repo gallery button
+const filterInput = document.querySelector('.filter-repos'); // filter/search by name input
 
 const username = 'MattPahuta';
 
@@ -50,6 +52,7 @@ const getRepos = async function() {
 
 // build the ul of li repo names
 const displayRepos = repos => {
+  filterInput.classList.remove('hide');
   for (const repo of repos) {
     const li = document.createElement('li');
     li.classList.add('repo');
@@ -65,6 +68,7 @@ repoList.addEventListener('click', (e) => {
     // console.log(repoName);
     getRepoInfo(repoName); // repoName.languages_url
   }
+  // filterInput.value = '';
 })
 
 // get specific repo data
@@ -87,7 +91,8 @@ const getRepoInfo = async repoName => {
 const displayRepoInfo = (repoInfo, languages) => {
   repoData.innerHTML = '';
   repoData.classList.remove('hide');
-  repos.classList.add('hide');
+  allReposContainer.classList.add('hide');
+  backBtn.classList.remove('hide'); // unhide the back to repo button
   const repoDiv = document.createElement('div');
   repoDiv.innerHTML = `
     <h3>Name: ${repoInfo.name}</h3>
@@ -97,3 +102,32 @@ const displayRepoInfo = (repoInfo, languages) => {
       <a class='visit' href='${repoInfo.svn_url}' target='_blank' rel='noreferrer noopener'>View Repo on GitHub!</a>`
   repoData.append(repoDiv);
 }
+
+// back button click event handler
+backBtn.addEventListener('click', () => {
+  allReposContainer.classList.remove('hide'); // unhide repos section
+  repoData.classList.add('hide'); // hide individual repo details
+  backBtn.classList.add('hide'); // hide back to repo galery button
+  // filterInput.value = '';
+})
+
+// dynamic repo search - input element
+filterInput.addEventListener('input', (e) => {
+  const searchText = e.target.value; // get the text from search input
+  const repos = document.querySelectorAll('.repo'); // all repo elements on the page
+  const searchTextLowerCase = searchText.toLowerCase(); // searchText lowercased
+
+  for (let repo of repos) { // loop over all the repos on the page
+    const repoTextLowerCase = repo.innerText.toLowerCase(); // get lowercased repo name
+    if (repoTextLowerCase.includes(searchTextLowerCase)) { // check for matches
+      repo.classList.remove('hide'); // show matching repos
+    } else {
+      repo.classList.add('hide'); // hide non-matching repos
+    }
+  }
+
+})
+
+// Ensure search filter box is reset on subsequent page loads
+filterInput.value = '';
+filterInput.ariaPlaceholder = 'Search by name';
